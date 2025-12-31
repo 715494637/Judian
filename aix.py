20e0369a79bcb9d496e8ae34c964208a5c70fba6
cofffeee <93782833+715494637@users.noreply.github.com>
2025-12-31 11:21:08 +0800

    Update aix.py

--- aix.py
+++ aix.py
@@ -6,20 +6,20 @@ headers = {
     "accept-language": "zh-CN,zh;q=0.9",
     "cache-control": "no-cache",
     "content-type": "application/json",
-    "origin": "https://aixldc01.cc",
+    "origin": "https://aixldc09.shop",
     "pragma": "no-cache",
     "priority": "u=1, i",
-    "referer": "https://aixldc01.cc/",
-    "sec-ch-ua": "\"Not)A;Brand\";v=\"8\", \"Chromium\";v=\"138\", \"Microsoft Edge\";v=\"138\"",
+    "referer": "https://aixldc09.shop/viewVideoRecommend?shareUserId=1950808184285294592",
+    "sec-ch-ua": "\"Microsoft Edge\";v=\"143\", \"Chromium\";v=\"143\", \"Not A(Brand\";v=\"24\"",
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": "\"Windows\"",
     "sec-fetch-dest": "empty",
     "sec-fetch-mode": "cors",
-    "sec-fetch-site": "cross-site",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0",
+    "sec-fetch-site": "same-origin",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36 Edg/143.0.0.0",
     "x-client-id": "vue-client"
 }
-url = "https://ld01.aixwebfx001.top/view/shareUserIncome/1950808184285294592"
+url = "https://aixldc09.shop/api/view/shareUserIncome/1950808184285294592"
 data = {
     "\"wjRJWK3baizagc4xo6bR4A": "=\""
 }
